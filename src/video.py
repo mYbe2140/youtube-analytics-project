7ffa3a58595dbc1198d5c751d394c9e896f7640c
Aleksander Garmashov <alexandr.mybe@mail.ru>
2023-07-30 01:46:02 +0700

    fixed __init__ video.py

--- src/video.py
+++ src/video.py
@@ -3,24 +3,39 @@ import os
 from googleapiclient.discovery import build
 
 
+class VideoIdError(Exception):
+    pass
+
+
 class Video:
 
     def __init__(self, video_id: str):
-        self.video_id = video_id
-        self.video_info = self.get_info()
-        self.video_name = self.video_info["items"][0]["snippet"]["title"]
-        self.video_link = f'https://www.youtube.com/watch?v={self.video_id}'
-        self.video_views = self.video_info["items"][0]["statistics"]["viewCount"]
-        self.video_likes = self.video_info["items"][0]["statistics"]["likeCount"]
+        try:
+            self.video_id = video_id
+            self.video_info = self.get_info()
+            self.title = self.video_info["items"][0]["snippet"]["title"]
+            self.video_link = f'https://www.youtube.com/watch?v={self.video_id}'
+            self.video_views = self.video_info["items"][0]["statistics"]["viewCount"]
+            self.like_count = self.video_info["items"][0]["statistics"]["likeCount"]
+        except VideoIdError:
+            self.video_info = None
+            self.title = None
+            self.video_link = None
+            self.video_views = None
+            self.like_count = None
+            print("Неверно указан id видео")
 
     def __str__(self):
-        return self.video_name
+        return self.title
 
     def get_info(self):
         api_key: str = os.getenv('API_KEY')
         youtube = build('youtube', 'v3', developerKey=api_key)
         video_info = youtube.videos().list(id=self.video_id, part='snippet,statistics,contentDetails,topicDetails').execute()
-        return video_info
+        if len(video_info['items']) == 0:
+            raise VideoIdError
+        else:
+            return video_info
 
 
 class PLVideo(Video):
